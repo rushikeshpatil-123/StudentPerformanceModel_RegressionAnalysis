f60c4f86d048e7825186871f99a4d1adc47ab94d
rushikeshpatil-123 <rushikeshpatil1600m@gmail.com>
2025-12-13 12:46:21 +0530

    Activate

--- main.py
+++ main.py
@@ -1,52 +1,31 @@
 # Import Data Manipulation Libraries
-
 import numpy as np 
 import pandas as pd 
 
-# Import Data Visulization Libraries
-
-import seaborn as sns
+# Import Data Visualization Libraries
+import seaborn as sns 
 import matplotlib.pyplot as plt 
 
-# Import Data Warnings Libraries
-import Warnings
-Warnings.filterwarnings(action = 'ignore')
-
+# Import Data Warning Libraries
+import warnings
+warnings.filterwarnings(action = 'ignore')
 
 # Import Scikit Learn Libraries
 from sklearn.preprocessing import MinMaxScaler,LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error,r2_score
+
+filepath = 'https://raw.githubusercontent.com/rushikeshpatil-123/StudentPerformanceModel_RegressionAnalysis/refs/heads/main/Data/Student_Performance.csv'
 
 
 # step1: Data Ingestion()
 
 def data_ingestion():
-    pass
-
-# step2: Data Exploration
-
-def data_exploration(df):
-    return num_stats,cat_stats,data_info
-
-# step3: Data Preprocessing()
-
-def data_preprocessing(df):
-    return X_train,X_test,y_train,y_test = data_preprocessing(df)
+    return pd.read_csv(filepath)
 
 
-
-
-
-
-# Function Calling
-# step 1: Data Ingestion 
+# function Calling 
 df = data_ingestion()
 
-# step 2: Checking Descriptive Stats
-num_stats,cat_stats,data_info = data_exploration(df)
-
-# Step 3: Data Preprocessing
-
-X_train,X_test,y_train,y_test = data_preprocessing(df)
-
+# Test 
+print(df)
